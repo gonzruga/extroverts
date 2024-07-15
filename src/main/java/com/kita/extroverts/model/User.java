f7d115c4671631307d72d1cff603c8494b07c0c8
gonzruga <grugambwa@gmail.com>
2024-07-15 15:23:49 +0300

    Commit SM about

--- src/main/java/com/kita/extroverts/model/User.java
+++ src/main/java/com/kita/extroverts/model/User.java
@@ -20,7 +20,7 @@ import java.util.Set;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name="id", nullable = false)
     private Long id;
 
